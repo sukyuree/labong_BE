f6ab76de9d5b4484029a411e1aba3ded27b25840
sukyuree@ajou.ac.kr <sukyuree@ajou.ac.kr>
2023-05-18 10:36:01 +0900

    프로젝트 생성

---
+++ labongBE/src/main/java/com/comealone/jeju/LabongBeApplication.java
@@ -0,0 +1,13 @@
+package com.comealone.jeju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabongBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabongBeApplication.class, args);
+	}
+
+}
